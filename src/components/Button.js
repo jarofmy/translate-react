d579a80e2c8a9103bb647c82bdbd5d15f3a4309e
Jaro <jeremyvan614@gmail.com>
2020-01-22 09:06:08 -0600

    add color context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Gửi đi';
+  };
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  };
 
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Gửi đi';
     return (
-      <button className="ui button primary">{text}</button>
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
     );
   };
 };
